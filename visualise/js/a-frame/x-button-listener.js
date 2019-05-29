90e8535adacb98b2a233b1207f97c6d49ab6a93a
Benjamin Marks <benjy.marks@sydney.edu.au>
2019-05-29 17:54:21 +1000

    Got clicking working! System now pauses on grip

--- visualise/js/a-frame/x-button-listener.js
+++ visualise/js/a-frame/x-button-listener.js
@@ -1,13 +1,12 @@
 AFRAME.registerComponent('x-button-listener', {
   init: function () {
     var el = this.el;
-    el.addEventListener('xbuttondown', function (evt) {
+    el.addEventListener('gripdown', function (evt) {
       el.setAttribute('visible', !el.getAttribute('visible'));
       console.log('X BUTTON PUSHED');
       var particles = document.querySelector("a-entity[particles]")
-      var paused = particles.getAttribute('paused');
-      particles.setAttribute('paused', 1 - paused);
-
+      var paused = particles.components.particles.data.paused;
+      particles.components.particles.data.paused = 1 - paused;
     });
   }
 });
