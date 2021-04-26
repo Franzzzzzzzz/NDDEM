aecd2fb1ee3804ad8d566cbf77f95c1b559cdd09
Franzzzzzzzz <francois.guillard@sydney.edu.au>
2021-04-26 12:08:53 +1000

    Small update/bugfix on CG

--- src/CoarseGraining/IOLiggghts.cpp
+++ src/CoarseGraining/IOLiggghts.cpp
@@ -228,19 +228,19 @@ int Datafile:: do_post_atm()
  sort(tdata.begin(), tdata.end(), [=](auto v1, auto v2) {return v1[idloc] < v2[idloc] ; }); //WARNING idx 0 should be the particle ID
  for (i=0, j=tdata.begin() ; i<N ; i++, j++)
  {
-  if (i+1<(*j)[0])
+  if (i+1<(*j)[idloc])
   {
     j=tdata.insert(j, nanvec) ;
-    (*j)[0]=i+1 ;
+    (*j)[idloc]=i+1 ;
     nadded++ ;
   }
  }
 
  for (i=0,j=tdata.begin() ; j<tdata.end() ; j++,i++)
  {
-   (*j)[0]-- ;
-   if ( (*j)[0] != i)
-     printf("ERR shouldn't happen %d %g\n", i, (*j)[0]) ;
+   (*j)[idloc]-- ;
+   if ( (*j)[idloc] != i)
+     printf("ERR shouldn't happen %d %g\n", i, (*j)[idloc]) ;
  }
  printf("%d atom null added\n", nadded) ;
  N+=nadded ;
