4466507c722f65867d195af6e32471514f24e324
Benjy Marks <benjy.marks@sydney.edu.au>
2022-02-07 23:09:01 +1100

    trying CI action again

--- CI_build_script.sh
+++ CI_build_script.sh
@@ -1,18 +1,15 @@
 mkdir build
 mkdir deploy
+mkdir Samples
+mkdir Textures
 cd build
 
 embuilder build boost_headers zlib
 emcmake cmake ../src
 
-emmake make DEMND -s USE_BOOST_HEADERS=1 -s USE_ZLIB=1
-cp ../bin/DEMND.js ../deploy/
-cp ../bin/DEMND.wasm ../deploy/
-
-emmake make CoarseGraining -s USE_BOOST_HEADERS=1 -s USE_ZLIB=1
-cp ../bin/CoarseGraining.js ../deploy/
-cp ../bin/CoarseGraining.wasm ../deploy/
-
-emmake make DEMCGND -s USE_BOOST_HEADERS=1 -s USE_ZLIB=1
-cp ../bin/DEMCGND.js ../deploy/
-cp ../bin/DEMCGND.wasm ../deploy/
+for BUILD_TARGET in DEMND CoarseGraining # DEMCGND
+do
+    emmake make $BUILD_TARGET -s USE_BOOST_HEADERS=1 -s USE_ZLIB=1
+    cp ../bin/$BUILD_TARGET.js ../deploy/
+    cp ../bin/$BUILD_TARGET.wasm ../deploy/
+done
