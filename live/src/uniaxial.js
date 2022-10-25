d38151f2de26f2f795ca32618f71559b81a3d184
Benjamin Marks <benjy.marks@sydney.edu.au>
2022-10-25 14:28:44 +1100

    updated damping value in uniaxial example

--- live/src/uniaxial.js
+++ live/src/uniaxial.js
@@ -331,7 +331,7 @@ function setup_NDDEM() {
     S.simu_interpret_command("set GammaT " + String(vals.dissipation));
     S.simu_interpret_command("set Mu 0.5");
     S.simu_interpret_command("set Mu_wall 0");
-    S.simu_interpret_command("set damping 1000"); // NOTE: ARTIFICAL DAMPING!!!
+    S.simu_interpret_command("set damping 0.001"); // NOTE: ARTIFICAL DAMPING!!!
     S.simu_interpret_command("set T 150");
     S.simu_interpret_command("set dt " + String(tc/20));
     S.simu_interpret_command("set tdump 1000000"); // how often to calculate wall forces
