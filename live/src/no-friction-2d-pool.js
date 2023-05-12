9e3054c2c06af00f71e478639f6c4efd3e83b1f6
benjym <benjy.marks@sydney.edu.au>
2023-05-12 06:21:56 +0000

    Deploying to gh-pages2 from @ Franzzzzzzzz/NDDEM@4e8d29547bbb859f48390a2b5b398ed947abe087 🚀

--- live/src/no-friction-2d-pool.js
+++ live/src/no-friction-2d-pool.js
@@ -30,15 +30,38 @@ reset_div.style.position = "absolute";
 reset_div.style.right = "15px";
 reset_div.style.top = "15px";
 reset_div.style.padding = "10px";
-// reset_div.onclick( reset_particle );
-document.body.appendChild(reset_div);
+// document.body.appendChild(reset_div);
 
-reset_div.addEventListener("click", reset_particle);
+// reset_div.addEventListener("click", reset_particle);
+
+let boundary_select = document.createElement("select");
+boundary_select.style.position = "absolute";
+boundary_select.style.right = "15px";
+boundary_select.style.top = "15px";
+boundary_select.style.font_size = "large";
+boundary_select.style.padding = "5px 10px";
+boundary_select.style.border_radius = "11px";
+boundary_select.style.text_align = "center";
+let options = ['Square',
+               'Circle',
+               'Triangle',
+            //    'Ellipse'
+            ];
+//Create and append the options
+for (var i = 0; i < options.length; i++) {
+    var option = document.createElement("option");
+    option.value = options[i];
+    option.text = options[i];
+    boundary_select.appendChild(option);
+}
+
+document.body.appendChild(boundary_select);
+boundary_select.addEventListener("change", update_boundary);
 
 
 var urlParams = new URLSearchParams(window.location.search);
 var clock = new THREE.Clock();
-
+let NDDEMCGLib;
 let camera, scene, renderer, stats, panel, controls;
 let gui;
 let S;
@@ -47,7 +70,7 @@ var params = {
     dimension: 2,
     L: 0.06, //system size
     N: 1,
-    zoom: 1.4,
+    zoom: 1.3,
     g_mag: 1e3,
     theta: 0, // slope angle in DEGREES
     d4: {cur:0},
@@ -69,7 +92,9 @@ var params = {
     aspect_ratio: undefined,
 }
 
-params.aspect_ratio = window.innerHeight / window.innerWidth;
+// params.aspect_ratio = window.innerHeight / window.innerWidth;
+let window_aspect_ratio = window.innerHeight / window.innerWidth;
+params.aspect_ratio = 1;
 
 params.average_radius = (params.r_min + params.r_max)/2.;
 params.thickness = params.average_radius;
@@ -93,21 +118,30 @@ async function init() {
 
     // camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
     // var aspect = window.innerWidth / window.innerHeight;
+    console.log(window_aspect_ratio);
+    let left, top;
+    if ( window_aspect_ratio > 1 ) {
+        left = params.L*params.zoom;
+        top = params.L*params.zoom*window_aspect_ratio;;
+    } else {
+        left = params.L*params.zoom/window_aspect_ratio;
+        top = params.L*params.zoom;
+    }
     camera = new THREE.OrthographicCamera(
-        -params.L*params.zoom,
-        params.L*params.zoom,
-        params.L*params.zoom*params.aspect_ratio,
-        -params.L*params.zoom*params.aspect_ratio,
+        -left,left,top,-top,
         -1000,
         1000
     );
     camera.position.set( 0, 0, -5*params.L );
-    camera.up.set(0, 0, 1);
+    camera.up.set(0, 1, 0);
     camera.lookAt(0,0,0);
     // console.log(camera)
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0x111111 );
+
+    update_boundary();
+
 
     const hemiLight = new THREE.HemisphereLight();
     hemiLight.intensity = 0.35;
@@ -119,27 +153,6 @@ async function init() {
     scene.add( dirLight );
 
     SPHERES.add_spheres(S,params,scene);
-
-    WALLS.add_back(params, scene);
-    WALLS.add_left(params, scene);
-    WALLS.add_right(params, scene);
-    WALLS.add_front(params, scene);
-    WALLS.back.scale.y = params.thickness;//Math.PI/2.;
-    // var vert_walls = [WALLS.left,WALLS.right,WALLS.back,WALLS.front];
-
-    // vert_walls.forEach( function(mesh) {
-    WALLS.left.scale.x = 2*params.L*params.aspect_ratio + 2*params.thickness;
-    WALLS.right.scale.x = 2*params.L*params.aspect_ratio + 2*params.thickness;
-
-    WALLS.back.scale.x = 2*params.L + 2*params.thickness;
-    WALLS.front.scale.x = 2*params.L + 2*params.thickness;
-
-    WALLS.back.position.x = -params.L*params.aspect_ratio - params.thickness/2.;
-    WALLS.front.position.x = params.L*params.aspect_ratio + params.thickness/2.;
-
-        // mesh.scale.z = 2*params.L + 2*params.thickness;
-    // });
-    WALLS.wall_material.wireframe = false;
 
     RAYCAST.add_ghosts(scene, 2000, params.average_radius/4., 0xeeeeee);
 
@@ -154,25 +167,28 @@ async function init() {
     container.appendChild( renderer.domElement );
 
     window.addEventListener( 'resize', onWindowResize, false );
+    
 
     animate();
 }
 
 function onWindowResize(){
+    window_aspect_ratio = window.innerHeight / window.innerWidth;
+    let left, top;
+    if ( window_aspect_ratio > 1 ) {
+        left = params.L*params.zoom;
+        top = params.L*params.zoom*window_aspect_ratio;;
+    } else {
+        left = params.L*params.zoom/window_aspect_ratio;
+        top = params.L*params.zoom;
+    }
+    camera.left = -left;
+    camera.right = left;
+    camera.bottom = -top;
+    camera.top = top;
 
-    // camera.aspect = window.innerWidth / window.innerHeight;
-    // camera.updateProjectionMatrix();
-    // var aspect = window.innerWidth / window.innerHeight;
-    // camera.left = -params.zoom * aspect;
-    // camera.right = params.zoom * aspect;
-    // camera.bottom = -params.zoom;
-    // camera.top = params.zoom;
-
-    // camera.left = -params.L*params.zoom;
-    // camera.right = params.L*params.zoom;
-    // camera.top = params.L*params.zoom*params.aspect_ratio;
-    // camera.bottom = -params.L*params.zoom*params.aspect_ratio;
-    // renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 
@@ -181,13 +197,82 @@ function animate() {
     SPHERES.move_spheres(S,params);
     RAYCAST.animate_locked_particle(S, camera, SPHERES.spheres, params);
 
-    S.simu_step_forward(5);
+    S.simu_step_forward(50);
     // CGHANDLER.update_2d_cg_field(S,params);
     RAYCAST.update_ghosts();
     // SPHERES.draw_force_network(S, params, scene);
     renderer.render( scene, camera );
-    if ( Math.abs(SPHERES.spheres.children[0].position.x) > params.L*params.aspect_ratio ) {S.simu_fixParticle(0,[0,0]); }
-    if ( Math.abs(SPHERES.spheres.children[0].position.y) > params.L ) {S.simu_fixParticle(0,[0,0]); }
+    // if ( Math.abs(SPHERES.spheres.children[0].position.x) > params.L*params.aspect_ratio ) {S.simu_fixParticle(0,[0,0]); }
+    // if ( Math.abs(SPHERES.spheres.children[0].position.y) > params.L ) {S.simu_fixParticle(0,[0,0]); }
+}
+
+function update_boundary() {
+    if ( S !== undefined ) {
+        S.simu_finalise();
+        reset_particle();
+    }
+    
+    if ( params.dimension == 2 ) {
+        S = new NDDEMCGLib.DEMCG2D (params.N);
+    }
+    else if ( params.dimension == 3 ) {
+        S = new NDDEMCGLib.DEMCG3D (params.N);
+    }
+    else if ( params.dimension == 4 ) {
+        S = new NDDEMCGLib.DEMCG4D (params.N);
+    }
+    else if ( params.dimension == 5 ) {
+        S = new NDDEMCGLib.DEMCG5D (params.N);
+    }
+    finish_setup();
+
+    WALLS.remove_all_walls(scene);
+
+    if ( boundary_select.value === "Square" ) {
+        console.log(scene);
+        WALLS.add_back(params, scene);
+        WALLS.add_left(params, scene);
+        WALLS.add_right(params, scene);
+        WALLS.add_front(params, scene);
+        WALLS.back.scale.y = params.thickness;
+        
+        WALLS.left.scale.x = 2*params.L*params.aspect_ratio + 2*params.thickness;
+        WALLS.right.scale.x = 2*params.L*params.aspect_ratio + 2*params.thickness;
+
+        WALLS.back.scale.x = 2*params.L + 2*params.thickness;
+        WALLS.front.scale.x = 2*params.L + 2*params.thickness;
+
+        WALLS.back.position.x = -params.L*params.aspect_ratio - params.thickness/2.;
+        WALLS.front.position.x = params.L*params.aspect_ratio + params.thickness/2.;
+
+    } else if ( boundary_select.value === 'Circle' ) {
+        params.R = params.L;
+        WALLS.add_circle_wall(params, scene);
+        console.log(WALLS.left);
+    } else if ( boundary_select.value === 'Triangle' ) {
+        let H = Math.sqrt(3*params.L*params.L);
+
+        WALLS.add_left(params, scene); // this is the bottom wall
+        WALLS.left.scale.x = 2*params.L + params.thickness;
+        WALLS.left.position.y = -H/2.;
+       
+        WALLS.add_back(params, scene); // this is the slanted wall on the right
+        WALLS.back.scale.x = 2*params.L + params.thickness;
+        WALLS.back.rotateZ(Math.PI/6.);
+        WALLS.back.position.x = params.L/2.;
+        WALLS.back.position.y = 0;
+
+        WALLS.add_front(params, scene); // this is the slanted wall on the right
+        WALLS.front.scale.x = 2*params.L + params.thickness;
+        WALLS.front.rotateZ(-Math.PI/6.);
+        WALLS.front.position.x = -params.L/2.;
+        WALLS.front.position.y = 0;
+        
+    } else if ( boundary_select.value === 'Ellipse' ) {
+
+    }
+    WALLS.wall_material.wireframe = false;
+    reset_particle();
 }
 
 function reset_particle(){
@@ -206,64 +291,66 @@ async function NDDEMCGPhysics() {
 
     }
 
-    await DEMCGND().then( (NDDEMCGLib) => {
-        if ( params.dimension == 2 ) {
-            S = new NDDEMCGLib.DEMCG2D (params.N);
-        }
-        else if ( params.dimension == 3 ) {
-            S = new NDDEMCGLib.DEMCG3D (params.N);
-        }
-        else if ( params.dimension == 4 ) {
-            S = new NDDEMCGLib.DEMCG4D (params.N);
-        }
-        else if ( params.dimension == 5 ) {
-            S = new NDDEMCGLib.DEMCG5D (params.N);
-        }
-        finish_setup();
+    await DEMCGND().then( (lib) => {
+        NDDEMCGLib = lib;
     } );
+}
 
 
-    function finish_setup() {
-        S.simu_interpret_command("dimensions " + String(params.dimension) + " " + String(params.N));
-        S.simu_interpret_command("radius -1 0.5");
-        let m;
-        if ( params.dimension === 2) {
-            m = Math.PI*0.5*0.5*params.particle_density;
-        } else {
-            m = 4./3.*Math.PI*0.5*0.5*0.5*params.particle_density;
-        }
+function finish_setup() {
+    S.simu_interpret_command("dimensions " + String(params.dimension) + " " + String(params.N));
+    S.simu_interpret_command("radius -1 0.5");
+    let m;
+    if ( params.dimension === 2) {
+        m = Math.PI*0.5*0.5*params.particle_density;
+    } else {
+        m = 4./3.*Math.PI*0.5*0.5*0.5*params.particle_density;
+    }
 
-        S.simu_interpret_command("mass -1 " + String(m));
-        S.simu_interpret_command("auto rho");
-        S.simu_interpret_command("auto radius uniform "+params.r_min+" "+params.r_max);
-        S.simu_interpret_command("auto mass");
-        S.simu_interpret_command("auto inertia");
-        S.simu_interpret_command("auto skin");
+    S.simu_interpret_command("mass -1 " + String(m));
+    S.simu_interpret_command("auto rho");
+    S.simu_interpret_command("auto radius uniform "+params.r_min+" "+params.r_max);
+    S.simu_interpret_command("auto mass");
+    S.simu_interpret_command("auto inertia");
+    S.simu_interpret_command("auto skin");
 
+    S.simu_interpret_command("gravity 0 " + "0 ".repeat(params.dimension - 2))
+
+    S.simu_interpret_command("location 0 0 0");
+
+    let tc = 1e-3;
+    let rest = 1.0 - 1e-10; // super low restitution coeff to dampen out quickly
+    let vals = SPHERES.setCollisionTimeAndRestitutionCoefficient (tc, rest, params.particle_mass)
+
+    S.simu_interpret_command("set Kn " + String(vals.stiffness));
+    S.simu_interpret_command("set Kt " + String(0.8*vals.stiffness));
+    S.simu_interpret_command("set GammaN " + String(vals.dissipation));
+    S.simu_interpret_command("set GammaT " + String(vals.dissipation));
+    S.simu_interpret_command("set Mu 0.0");
+    S.simu_interpret_command("set Mu_wall 0.0");
+    // S.simu_interpret_command("set damping 0.00001");
+    S.simu_interpret_command("set T 150");
+    S.simu_interpret_command("set dt " + String(tc/20));
+    S.simu_interpret_command("set tdump 1000000"); // how often to calculate wall forces
+    S.simu_interpret_command("auto skin");
+
+    if ( boundary_select.value === "Square" ) {
         S.simu_interpret_command("boundary 0 WALL -"+String(params.L*params.aspect_ratio)+" "+String(params.L*params.aspect_ratio));
         S.simu_interpret_command("boundary 1 WALL -"+String(params.L)+" "+String(params.L));
-        S.simu_interpret_command("gravity 0 " + "0 ".repeat(params.dimension - 2))
 
-        S.simu_interpret_command("location 0 0 0");
+    } else if ( boundary_select.value === "Circle" ) { 
+        S.simu_interpret_command("boundary "+String(params.dimension)+" SPHERE "+String(params.L)+" 0 0"); // add a sphere!
+        S.simu_interpret_command("boundary 0 WALL -"+String(2*params.L)+" "+String(2*params.L));
+        S.simu_interpret_command("boundary 1 WALL -"+String(2*params.L)+" "+String(2*params.L));
+    } else if ( boundary_select.value === "Ellipse" ) {
 
-        let tc = 1e-2;
-        let rest = 1.0 - 1e-10; // super low restitution coeff to dampen out quickly
-        let vals = SPHERES.setCollisionTimeAndRestitutionCoefficient (tc, rest, params.particle_mass)
-
-        S.simu_interpret_command("set Kn " + String(vals.stiffness));
-        S.simu_interpret_command("set Kt " + String(0.8*vals.stiffness));
-        S.simu_interpret_command("set GammaN " + String(vals.dissipation));
-        S.simu_interpret_command("set GammaT " + String(vals.dissipation));
-        S.simu_interpret_command("set Mu 0.0");
-        S.simu_interpret_command("set Mu_wall 0.0");
-        S.simu_interpret_command("set damping 0.00001");
-        S.simu_interpret_command("set T 150");
-        S.simu_interpret_command("set dt " + String(tc/20));
-        S.simu_interpret_command("set tdump 1000000"); // how often to calculate wall forces
-        S.simu_interpret_command("auto skin");
-        S.simu_finalise_init () ;
-
-        
-
+    } else if ( boundary_select.value === "Triangle" ) {
+        let H = Math.sqrt(3*params.L*params.L);
+        S.simu_interpret_command("location 0 0 "+String(-H/6.));
+        S.simu_interpret_command('mesh string {"dimension":2,"objects":[{"dimensionality":1,"vertices":[['+String(-params.L)+','+String(-H/2.)+'],[0,'+String(H/2)+']]},{"dimensionality":1,"vertices":[['+String(params.L)+','+String(-H/2.)+'],[0,'+String(H/2.)+']]}]}');
+        S.simu_interpret_command("boundary 0 WALL -"+String(10*params.L)+" "+String(10*params.L));
+        S.simu_interpret_command("boundary 1 WALL -"+String(H/2.)+" "+String(10*params.L));
     }
+
+    S.simu_finalise_init () ;
 }
